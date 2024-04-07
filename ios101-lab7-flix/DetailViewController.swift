ae1d138c9693e629965f78ce2a847ca0814f57dc
Muhammad Akbar <muhammadakbarwork@gmail.com>
2024-04-07 16:49:43 -0700

    added favorite and un-favorite any movie and the favorite button should show in its correct favorited / un-favorited state

--- ios101-lab7-flix/DetailViewController.swift
+++ ios101-lab7-flix/DetailViewController.swift
@@ -20,6 +20,15 @@ class DetailViewController: UIViewController {
     
     @IBAction func didTapFavoriteButton(_ sender: UIButton) {
         sender.isSelected = !sender.isSelected
+        // 1. If the button is in the *selected* state (i.e. "favorited") -> Add movie to favorites
+        // 2. Otherwise, the button is in the *un-selected* state (i.e."un-favorited") -> Remove movie from favorites
+        if sender.isSelected {
+            // 1.
+            movie.addToFavorites()
+        } else {
+            // 2.
+            movie.removeFromFavorites()
+        }
     }
     // TODO: Add favorite button outlet
 
@@ -33,9 +42,23 @@ class DetailViewController: UIViewController {
 
         // TODO: Update favorite button selected state
 
+        // Update the button's selected state based on the current movie's favorited status.
+        // 1. Get the array of favorite movies.
+        // 2. Check if the favorite movies array contains the current movie.
+        // 3. If so, the movie has been favorited -> Set the button to the *selected* state.
+        // 4. Otherwise, the movie is not-favorited -> Set the button to the *un-selected* state.
+        // ------
 
-
-
+        // 1.
+        let favorites = Movie.getMovies(forKey: Movie.favoritesKey)
+        // 2.
+        if favorites.contains(movie) {
+            // 3.
+            favoriteButton.isSelected = true
+        } else {
+            // 4.
+            favoriteButton.isSelected = false
+        }
         // MARK: Style views
         posterImageView.layer.cornerRadius = 20
         posterImageView.layer.borderWidth = 2
